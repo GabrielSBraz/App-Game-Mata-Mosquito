b8b68749af656b413d638338e3ab75b2fff7e802
Gabriel de Sousa Braz <gabrielbraz014@outlook.com>
2020-10-29 09:26:17 -0300

    Posição ramdômicas
    Utilização de elemelentos HTML no arquivo JavaScript e uma função para gerar posições aleatórias do mosquito na tela sem ultrapassar o tamanho dela.

--- jogo.js
+++ jogo.js
@@ -4,8 +4,32 @@ var largura = 0
 function ajustaTamanhoPalcoJogo() {
 	altura = window.innerHeight
 	largura = window.innerWidth
-	console.log(altura, largura)
+	console.log(largura, altura)
 }
 
 ajustaTamanhoPalcoJogo()
 
+function posicaoRandomica() {
+
+	var posicaoX = Math.floor(Math.random() * largura) - 90
+	var posicaoY = Math.floor(Math.random() * altura) - 90
+
+	posicaoX = posicaoX < 0 ? 0 : posicaoX
+	posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+	console.log(posicaoX, posicaoY)
+
+	//criar elemento html
+	var mosquito = document.createElement('img')
+	mosquito.src = 'imagens/mosca.png'
+	mosquito.className = 'mosquito1'
+	mosquito.style.left = posicaoX + 'px'
+	mosquito.style.top = posicaoY + 'px'
+	mosquito.style.position = 'absolute'
+
+	document.body.appendChild(mosquito)
+}
+
+function tamanhoAleatorio() {
+	var classe = Math.random() * 3
+}
